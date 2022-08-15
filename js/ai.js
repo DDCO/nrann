131263a01c6c7a54eb1e03ecc3d4a78852f469c1
DDCO <dan.da.cruz.oliveira@gmail.com>
2022-08-15 17:40:39 -0400

    end of day commit

--- js/ai.js
+++ js/ai.js
@@ -1,3 +1,114 @@
+const IMAGE_WIDTH = 28;
+const IMAGE_HEIGHT = 28;
+const IMAGE_CHANNELS = 1;
+
+function getModel() {
+  const model = tf.sequential();
+    
+  // In the first layer of our convolutional neural network we have 
+  // to specify the input shape. Then we specify some parameters for 
+  // the convolution operation that takes place in this layer.
+  model.add(tf.layers.conv2d({
+    inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+    kernelSize: 5,
+    filters: 8,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: 'varianceScaling'
+  }));
+  
+  // The MaxPooling layer acts as a sort of downsampling using max values
+  // in a region instead of averaging.  
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+    
+  // Repeat another conv2d + maxPooling stack. 
+  // Note that we have more filters in the convolution.
+  model.add(tf.layers.conv2d({
+    kernelSize: 5,
+    filters: 16,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: 'varianceScaling'
+  }));
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+    
+  // Now we flatten the output from the 2D filters into a 1D vector to prepare
+  // it for input into our last layer. This is common practice when feeding
+  // higher dimensional data to a final classification output layer.
+  model.add(tf.layers.flatten());
+  
+  // Our last layer is a dense layer which has 10 output units, one for each
+  // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
+  const NUM_OUTPUT_CLASSES = 10;
+  model.add(tf.layers.dense({
+    units: NUM_OUTPUT_CLASSES,
+    kernelInitializer: 'varianceScaling',
+    activation: 'softmax'
+  }));
+  
+    
+  // Choose an optimizer, loss function and accuracy metric,
+  // then compile and return the model
+  const optimizer = tf.train.adam();
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
+  });
+  
+  return model;
+}
+
+const model = getModel();
+
+async function train() {
+  const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
+  const container = {
+    name: 'Model Training', tab: 'Model', styles: { height: '1000px' }
+  };
+  
+  const BATCH_SIZE = 512;
+  const TRAIN_DATA_SIZE = 5500;
+  const TEST_DATA_SIZE = 1000;
+
+  const [trainXs, trainYs] = tf.tidy(() => {
+    const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+    return [
+      d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
+      d.labels
+    ];
+  });
+
+  const [testXs, testYs] = tf.tidy(() => {
+    const d = data.nextTestBatch(TEST_DATA_SIZE);
+    return [
+      d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
+      d.labels
+    ];
+  });
+
+  return model.fit(trainXs, trainYs, {
+    batchSize: BATCH_SIZE,
+    validationData: [testXs, testYs],
+    epochs: 10,
+    shuffle: true,
+    // callbacks: fitCallbacks
+  });
+}
+
 function classify (image) {
- return Math.floor(Math.random() * 10);
+  const IMAGE_SIZE = image.data.length / 4;
+  const d = new Float32Array(IMAGE_SIZE);
+  const l = new Uint8Array(10);
+  for (let j = 0; j < IMAGE_SIZE; j++) {
+    d[j] = image.data[(j * 4) + 3] / 255;
+  }
+  const testData = data.nextBatch(2, [d, l]);
+
+  const testxs = testData.xs.reshape([2, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+  const labels = testData.labels.argMax(-1);
+  const predictions = model.predict(testxs).argMax(-1);
+
+  testxs.dispose();
+  return predictions.arraySync()[0];
 };
--- js/draw.js
+++ js/draw.js
@@ -1,8 +1,10 @@
 const canvas = document.getElementById('myCanvas');
 const context = canvas.getContext("2d");
 const clearBtn = document.getElementById('clearBtn');
+const trainBtn = document.getElementById('trainBtn');
 const result = document.getElementById('result');
 let isDrawing = false;
+const isDebug = false;
 
 function draw (x, y) {
     context.beginPath();
@@ -11,35 +13,55 @@ function draw (x, y) {
 }
 
 function getImage () {
-    return context.getImageData(0, 0, canvas.width, canvas.height);
+    context.drawImage(canvas, 0, 0, canvas.width * 0.1, canvas.height * 0.1);
+    return context.getImageData(0, 0, 28, 28);
 }
 
 canvas.addEventListener('mousedown', (event) => {
     isDrawing = true;
-    console.log(event);
+    if (isDebug) {
+        console.log(event);
+    }
 });
 
 canvas.addEventListener('mouseup', (event) => {
     isDrawing = false;
     const image = getImage();
     result.innerText = classify(image);
-    console.log(event);
+    if (isDebug) {
+        console.log(event);
+    }
 });
 
 canvas.addEventListener('mousemove', (event) => {
     if (isDrawing) {
         draw(event.offsetX, event.offsetY);
     }
-    console.log(event);
+    if (isDebug) {
+        console.log(event);
+    }
 });
 
 canvas.addEventListener('mouseout', (event) => {
     isDrawing = false;
-    console.log(event);
+    if (isDebug) {
+        console.log(event);
+    }
 });
 
 clearBtn.addEventListener('click', (event) => {
     context.clearRect(0, 0, canvas.width, canvas.height);
     result.innerText = '-';
-    console.log(event);
-})
+    if (isDebug) {
+        console.log(event);
+    }
+});
+
+trainBtn.addEventListener('click', async (event) => {
+    const fit = await train();
+    alert('Model successfully trained');
+    console.log(fit);
+    if (isDebug) {
+        console.log(event);
+    }
+});
